7ba19830b500df33cfa1c5727fec5f09a72fe882
chlovers <chloie_day@hotmail.com>
2024-11-28 21:21:59 -0400

    added script for timeline

---
+++ Assets/Timeline Script.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Timelinestuff : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        timeline.Play();
+
+    }
+
+
+}
